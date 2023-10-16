a404004f09b0f8936c3204a636bed70440e14f29
Hamza Khan <hkhan8624@gmail.com>
2023-10-16 10:26:19 +0500

    fixing ui

--- Apeek.NH.Repository/Repositories/Impl/RepositoryTwilioMessage.cs
+++ Apeek.NH.Repository/Repositories/Impl/RepositoryTwilioMessage.cs
@@ -14,7 +14,7 @@ namespace Apeek.NH.Repository.Repositories.Impl
         {
             // For 8am  time to 9 pm 
             var resetAtZero = dateTime.Date;
-            var startTimeUtc = resetAtZero.Date.AddHours(7);//7 // 8 AM in UTC
+            var startTimeUtc = resetAtZero.Date.AddHours(7);// 8 AM in UTC
             var endTimeUtc = resetAtZero.Date.AddHours(21);   // 9 PM in UTC
             var count = Table.Where(x => x.Status == true && x.CreateDate.Date == dateTime).Count();
             if (dateTime > startTimeUtc && dateTime < endTimeUtc && count <= 100) return true;
